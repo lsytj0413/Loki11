b42aecfbe553ee50df60eaa5911fb3d24b0a0314
lsytj0413 <511121939@qq.com>
2016-10-21 13:53:32 +0800

    Field

---
+++ Loki11/HierarchyGenerator.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "Typelist.hpp"
+#include "TypeManip.hpp"
+
+
+namespace Loki11
+{
+
+template <typename T, template <class> class Unit>
+class GenScatterHierarchy;
+
+template <typename T, template <class> class Unit>
+class GenScatterHierarchy : public Unit<T>
+{
+public:
+    using LeftBase = Unit<T>;
+};
+
+template <template <class> class Unit>
+class GenScatterHierarchy<Typelist<>, Unit>
+{};
+
+template <typename Head, typename... TArgs, template <class> class Unit>
+class GenScatterHierarchy<Typelist<Head, TArgs...>, Unit>
+        : public GenScatterHierarchy<Head, Unit>
+        , public GenScatterHierarchy<Typelist<TArgs...>, Unit>
+{
+public:
+    using LeftBase = GenScatterHierarchy<Head, Unit>;
+    using RightBase = GenScatterHierarchy<Typelist<TArgs...>, Unit>;
+};
+
+template <class T, class... TArgs, template <class> class Unit>
+Unit<T>& Field(GenScatterHierarchy<Typelist<TArgs...>, Unit>& obj)
+{
+    return obj;
+};
+
+template <template <class> class Unit, typename Head, typename... TArgs>
+Unit<Head>& FieldHelper(GenScatterHierarchy<Typelist<Head, TArgs...>, Unit>& obj,
+                        Int2Type<0>)
+{
+    GenScatterHierarchy<Typelist<Head, TArgs...>, Unit>& leftBase = obj;
+    return leftBase;
+};
+
+template <int i, template <class> class Unit, typename Head, typename... TArgs>
+Unit<Head>& FieldHelper(GenScatterHierarchy<Typelist<Head, TArgs...>, Unit>& obj,
+                        Int2Type<i>)
+{
+    GenScatterHierarchy<Typelist<TArgs...>, Unit>& rightBase = obj;
+    return FieldHelper(rightBase, Int2Type<i-1>());
+};
+
+template <int i, class... TArgs, template <class> class Unit>
+Unit<typename TypeAt<Typelist<TArgs...>, i>::Result>&
+Field(GenScatterHierarchy<Typelist<TArgs...>, Unit>& obj)
+{
+    return FieldHelper(obj, Int2Type<i>());
+};
+
+
+
+}
