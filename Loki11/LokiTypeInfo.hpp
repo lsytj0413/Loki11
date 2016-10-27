244eecdc8cd1c805c5c695df3474e8c642d14136
lsytj0413 <511121939@qq.com>
2016-10-27 17:38:00 +0800

    comment

--- Loki11/LokiTypeInfo.hpp
+++ Loki11/LokiTypeInfo.hpp
@@ -1,3 +1,9 @@
+// @file LokiTypeInfo.hpp
+// @brief std::type_index的封装
+// @author
+// @version
+// @date
+
 #pragma once
 
 #include <typeinfo>
@@ -7,7 +13,9 @@
 namespace Loki11
 {
 
-// C++11之后可以使用std::type_index
+// @class TypeInfo
+// @brief 封装std::type_index
+// @c++11 使用std::type_index
 class TypeInfo
 {
 public:
@@ -36,16 +44,26 @@ public:
         return *this;
     };
 
+    // @function
+    // @brief 判断类型次序
+    // @param rhs: 另一个TypeInfo对象
+    // @return bool
     bool before(const TypeInfo& rhs) const {
         assert(pInfo_);
         return pInfo_->before(*rhs.pInfo_) != 0;
     };
 
+    // @function
+    // @brief 获取类型名称
+    // @return const char*
     const char* name() const {
         assert(pInfo_);
         return pInfo_->name();
     };
 
+    // @function
+    // @brief 获取所表示的std::type_index
+    // @return const std::type_index&
     const std::type_info& Get() const {
         assert(pInfo_);
         return *pInfo_;
@@ -55,31 +73,61 @@ private:
     const std::type_info* pInfo_;
 };
 
+// @function
+// @brief 判断两个TypeInfo是否相等
+// @param lhs: TypeInfo对象
+// @param rhs: TypeInfo对象
+// @return bool
 inline bool operator==(const TypeInfo& lhs, const TypeInfo& rhs)
 {
     return (lhs.Get() == rhs.Get()) != 0;
 }
 
+// @function
+// @brief 判断两个TypeInfo是否小于关系
+// @param lhs: TypeInfo对象
+// @param rhs: TypeInfo对象
+// @return bool
 inline bool operator<(const TypeInfo& lhs, const TypeInfo& rhs)
 {
     return lhs.before(rhs);
 }
 
+// @function
+// @brief 判断两个TypeInfo是否不相等
+// @param lhs: TypeInfo对象
+// @param rhs: TypeInfo对象
+// @return bool
 inline bool operator!=(const TypeInfo& lhs, const TypeInfo& rhs)
 {
     return !(lhs == rhs);
 }
 
+// @function
+// @brief 判断两个TypeInfo是否大于关系
+// @param lhs: TypeInfo对象
+// @param rhs: TypeInfo对象
+// @return bool
 inline bool operator>(const TypeInfo& lhs, const TypeInfo& rhs)
 {
     return rhs < lhs;
 }
 
+// @function
+// @brief 判断两个TypeInfo是否小于等于
+// @param lhs: TypeInfo对象
+// @param rhs: TypeInfo对象
+// @return bool
 inline bool operator<=(const TypeInfo& lhs, const TypeInfo& rhs)
 {
     return !(lhs > rhs);
 }
 
+// @function
+// @brief 判断两个TypeInfo是否大于等于
+// @param lhs: TypeInfo对象
+// @param rhs: TypeInfo对象
+// @return bool
 inline bool operator>=(const TypeInfo& lhs, const TypeInfo& rhs)
 {
     return !(lhs < rhs);
--- Loki11/TypeManip.hpp
+++ Loki11/TypeManip.hpp
@@ -14,11 +14,14 @@ namespace Loki11
 
 // @struct Int2Type
 // @brief 将整数转换为类型
-// @c++11 o使用std::integral_constant
+// @c++11 使用std::integral_constant
 template <int v>
 struct Int2Type : public std::integral_constant<int, v>
 {};
 
+
+// @struct Type2Type
+// @brief 将类型转换为类型
 template <typename T>
 struct Type2Type
 {
@@ -26,7 +29,9 @@ struct Type2Type
 };
 
 
-// Select, C++11之后可以使用std::conditional
+// @struct Select
+// @brief 编译期if, 类型选择
+// @c++11 使用std::conditional
 template <bool flag, typename T, typename U>
 struct Select
 {
@@ -47,6 +52,9 @@ struct ConversionImp
     static std::false_type Test(...);
 };
 
+// @struct Conversion
+// @brief 判断类型是否可隐式转换
+// @c++11 使用std::is_convertible
 template <class T, class U>
 class Conversion : public std::integral_constant<bool,
                                                  std::is_same<decltype(ConversionImp<U>::Test(std::declval<T>())),
@@ -54,8 +62,16 @@ class Conversion : public std::integral_constant<bool,
                                                  >
 {
 public:
+    // @variable
+    // @brief T是否能转换为U
     static const bool exists = Conversion<T, U>::value;
+
+    // @variable
+    // @brief 是否可以互相转换
     static const bool exists2Way = exists && Conversion<U, T>::value;
+
+    // @variable
+    // @brief 是否为同一类型
     static const bool sameType = std::is_same<T, U>::value;
 };
 
@@ -81,12 +97,15 @@ class Conversion<void, void> : public ConversionBase<true>
 {};
 
 
-// C++11: 可以使用std::is_base_of
-// 测试T是否是U的父类
+// @macro SUPERSUBCLASS
+// @brief 测试T是否是U的父类
+// @c++11 使用std::is_base_of
 #define SUPERSUBCLASS(T, U) \
     (Conversion<const U*, const T*>::exists && \
      !std::is_same<const T*, const void*>::value)
 
+// @macro SUPERSUBCLASS_STRICT
+// @brief 测试T是否是U的父类, 并排除T==U
 #define SUPERSUBCLASS_STRICT(T, U) \
     (SUPERSUBCLASS(T, U) && \
      !std::is_same<const T, const U>::value)
