0b86f591abdebb9f0b81d5f9d46b5e10dda8bb17
lsytj0413 <511121939@qq.com>
2016-10-28 17:39:57 +0800

    HierarchyGenTest init

---
+++ test/src/hierarchyGeneratorTest.cpp
@@ -0,0 +1,23 @@
+#include "hierarchyGeneratorTest.h"
+
+#include <Loki11/HierarchyGenerator.hpp>
+
+
+TEST_F(HierarchyGenTest, testScatter)
+{}
+
+
+TEST_F(HierarchyGenTest, testFieldT)
+{}
+
+
+TEST_F(HierarchyGenTest, testFieldI)
+{}
+
+
+TEST_F(HierarchyGenTest, testTuple)
+{}
+
+
+TEST_F(HierarchyGenTest, testLinear)
+{}
---
+++ test/src/hierarchyGeneratorTest.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <gtest/gtest.h>
+
+
+class HierarchyGenTest : public testing::Test
+{};
