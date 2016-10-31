f92afc8ee08cf2a517c1edaaf1dfec09ce03372b
lsytj0413 <511121939@qq.com>
2016-10-31 13:31:21 +0800

    FieldHelper

--- Loki11/HierarchyGenerator.hpp
+++ Loki11/HierarchyGenerator.hpp
@@ -13,10 +13,24 @@
 namespace Loki11
 {
 
+
 // @class GenScatterHierarchy
 // @brief 散乱继承模板
 template <typename T, template <class> class Unit>
 class GenScatterHierarchy;
+
+namespace detail
+{
+
+template <typename T, template <class> class Unit>
+class ScatterHierarchySign;
+
+template <typename Head, typename... TArgs, template <class> class Unit>
+class ScatterHierarchySign<Typelist<Head, TArgs...>, Unit>
+        : public GenScatterHierarchy<Head, Unit>
+{};
+
+}
 
 template <typename T, template <class> class Unit>
 class GenScatterHierarchy : public Unit<T>
@@ -31,47 +45,33 @@ class GenScatterHierarchy<Typelist<>, Unit>
 
 template <typename Head, typename... TArgs, template <class> class Unit>
 class GenScatterHierarchy<Typelist<Head, TArgs...>, Unit>
-        : public GenScatterHierarchy<Head, Unit>
+        : public detail::ScatterHierarchySign<Typelist<Head, TArgs...>, Unit>
         , public GenScatterHierarchy<Typelist<TArgs...>, Unit>
 {
 public:
-    using LeftBase = GenScatterHierarchy<Head, Unit>;
+    using LeftBase = detail::ScatterHierarchySign<Typelist<Head, TArgs...>, Unit>;
     using RightBase = GenScatterHierarchy<Typelist<TArgs...>, Unit>;
 };
 
-
-// @function
-// @brief 将散乱生成的类转换为基类类型(按类型)
-// @param obj: 生成类
-// @return Unit<T>的引用
-template <class T, class... TArgs, template <class> class Unit>
-Unit<T>& Field(GenScatterHierarchy<Typelist<TArgs...>, Unit>& obj)
-{
-    return obj;
-};
-
-template <class T, class... TArgs, template <class> class Unit>
-const Unit<T>& Field(const GenScatterHierarchy<Typelist<TArgs...>, Unit>& obj)
-{
-    return obj;
-};
 
 namespace detail
 {
 
 template <template <class> class Unit, typename Head, typename... TArgs>
 Unit<Head>& FieldHelper(GenScatterHierarchy<Typelist<Head, TArgs...>, Unit>& obj,
-                        Int2Type<0>)
+                        const Int2Type<0>)
 {
-    GenScatterHierarchy<Typelist<Head, TArgs...>, Unit>& leftBase = obj;
+    typename GenScatterHierarchy<Typelist<Head, TArgs...>, Unit>::LeftBase& leftBase = obj;
     return leftBase;
 };
 
 template <int i, template <class> class Unit, typename Head, typename... TArgs>
-Unit<Head>& FieldHelper(GenScatterHierarchy<Typelist<Head, TArgs...>, Unit>& obj,
-                        Int2Type<i>)
+Unit<typename TypeAt<Typelist<Head, TArgs...>, i>::Result>&
+FieldHelper(GenScatterHierarchy<Typelist<Head, TArgs...>, Unit>& obj,
+                        const Int2Type<i>)
 {
     GenScatterHierarchy<Typelist<TArgs...>, Unit>& rightBase = obj;
+    // typename GenScatterHierarchy<Typelist<Head, TArgs...>, Unit>::RightBase& rightBase = obj;
     return FieldHelper(rightBase, Int2Type<i-1>());
 };
 
@@ -88,6 +88,37 @@ Field(GenScatterHierarchy<Typelist<TArgs...>, Unit>& obj)
 {
     return detail::FieldHelper(obj, Int2Type<i>());
 };
+
+template <int i, class... TArgs, template <class> class Unit>
+const Unit<typename TypeAt<Typelist<TArgs...>, i>::Result>&
+Field(const GenScatterHierarchy<Typelist<TArgs...>, Unit>& obj)
+{
+    return detail::FieldHelper(
+        const_cast<GenScatterHierarchy<Typelist<TArgs...>, Unit>&>(obj),
+        Int2Type<i>());
+};
+
+
+// @function
+// @brief 将散乱生成的类转换为基类类型(按类型)
+// @param obj: 生成类
+// @return Unit<T>的引用
+template <class T, class... TArgs, template <class> class Unit>
+Unit<T>& Field(GenScatterHierarchy<Typelist<TArgs...>, Unit>& obj)
+{
+    return Field<
+        IndexOf<Typelist<TArgs...>, T>::value
+        >(obj);
+};
+
+template <class T, class... TArgs, template <class> class Unit>
+const Unit<T>& Field(const GenScatterHierarchy<Typelist<TArgs...>, Unit>& obj)
+{
+    return Field<
+        IndexOf<Typelist<TArgs...>, T>::value
+        >(obj);
+};
+
 
 namespace detail
 {
--- test/src/hierarchyGeneratorTest.cpp
+++ test/src/hierarchyGeneratorTest.cpp
@@ -1,5 +1,8 @@
 #include "hierarchyGeneratorTest.h"
 
+#include <iostream>
+using namespace std;
+
 #include <Loki11/HierarchyGenerator.hpp>
 using namespace Loki11;
 
@@ -29,7 +32,7 @@ TEST_F(HierarchyGenTest, testScatterOne)
     auto v = std::is_same<int, decltype(v1.i)>::value;
     EXPECT_EQ(v, true);
 
-    v = std::is_same<GenScatterHierarchy<int, ScatUnit>, typename T1::LeftBase>::value;
+    v = std::is_same<detail::ScatterHierarchySign<Typelist<int>, ScatUnit>, typename T1::LeftBase>::value;
     EXPECT_EQ(v, true);
 
     using T2 = GenScatterHierarchy<int, ScatUnit>;
@@ -47,52 +50,102 @@ TEST_F(HierarchyGenTest, testScatterOne)
 TEST_F(HierarchyGenTest, testScatterMulti)
 {
     using T1 = GenScatterHierarchy<Typelist<int, char>, ScatUnit>;
-    using T2 = GenScatterHierarchy<int, ScatUnit>;
+    using T2 = detail::ScatterHierarchySign<Typelist<int, char>, ScatUnit>;
     using T3 = GenScatterHierarchy<Typelist<char>, ScatUnit>;
+    using T11 = ScatUnit<int>;
 
     auto v = std::is_same<T2, T1::LeftBase>::value;
     EXPECT_EQ(v, true);
     v = std::is_same<T3, T1::RightBase>::value;
     EXPECT_EQ(v, true);
+    v = std::is_same<T2::LeftBase, T11>::value;
+    EXPECT_EQ(v, true);
 
     using T4 = GenScatterHierarchy<Typelist<int, char, double>, ScatUnit>;
-    using T5 = GenScatterHierarchy<int, ScatUnit>;
+    using T5 = detail::ScatterHierarchySign<Typelist<int, char, double>, ScatUnit>;
     using T6 = GenScatterHierarchy<Typelist<char, double>, ScatUnit>;
+    using T41 = ScatUnit<int>;
 
     v = std::is_same<T5, T4::LeftBase>::value;
     EXPECT_EQ(v, true);
     v = std::is_same<T6, T4::RightBase>::value;
     EXPECT_EQ(v, true);
+    v = std::is_same<T41, T5::LeftBase>::value;
+    EXPECT_EQ(v, true);
 }
+
+
+static char g_index = 0;
+
+template <typename T>
+struct ScatUnitField
+{
+public:
+    T i;
+
+    ScatUnitField() {
+        i = g_index++;
+    }
+};
 
 
 TEST_F(HierarchyGenTest, testFieldT)
 {
-    using T1 = GenScatterHierarchy<Typelist<int, char, double>, ScatUnit>;
+    g_index = 0;
+
+    using T1 = GenScatterHierarchy<Typelist<int, char, double>, ScatUnitField>;
     auto v = T1();
 
     auto v1 = Field<int>(v);
     auto b = std::is_same<int, decltype(v1.i)>::value;
     EXPECT_EQ(b, true);
+    EXPECT_EQ(v1.i, 0);
 
-    auto v2 = Field<char>(T1());
+    auto v2 = Field<char>(v);
     b = std::is_same<char, decltype(v2.i)>::value;
     EXPECT_EQ(b, true);
+    EXPECT_EQ(v2.i, 1);
 
     auto v3 = Field<double>(v);
     b = std::is_same<double, decltype(v3.i)>::value;
     EXPECT_EQ(b, true);
+    EXPECT_EQ(v3.i, 2);
 
-    // compile error:  ambiguous base
-    // using T2 = GenScatterHierarchy<Typelist<int, char, int>, ScatUnit>;
-    // auto v4 = Field<int>(T2());
-    // b = std::is_same<double, decltype(v4.i)>::value;
-    // EXPECT_EQ(b, true);
+    g_index = 0;
+    using T2 = GenScatterHierarchy<Typelist<int, char, int>, ScatUnitField>;
+    auto v4 = Field<int>(T2());
+    b = std::is_same<int, decltype(v4.i)>::value;
+    EXPECT_EQ(b, true);
 }
 
 
 TEST_F(HierarchyGenTest, testFieldI)
-{}
+{
+    g_index = 0;
+
+    using T1 = GenScatterHierarchy<Typelist<int, char, double, int>, ScatUnitField>;
+    auto v = T1();
+
+    auto v1 = Field<0>(v);
+    auto b = std::is_same<int, decltype(v1.i)>::value;
+    EXPECT_EQ(b, true);
+    EXPECT_EQ(v1.i, 0);
+
+    auto v2 = Field<1>(v);
+    b = std::is_same<char, decltype(v2.i)>::value;
+    EXPECT_EQ(b, true);
+    EXPECT_EQ(v2.i, 1);
+
+    auto v3 = Field<2>(v);
+    b = std::is_same<double, decltype(v3.i)>::value;
+    EXPECT_EQ(b, true);
+    EXPECT_EQ(v3.i, 2);
+
+    auto v4 = Field<3>(v);
+    b = std::is_same<int, decltype(v4.i)>::value;
+    EXPECT_EQ(b, true);
+    EXPECT_EQ(v4.i, 3);
+}
 
 
 TEST_F(HierarchyGenTest, testTuple)
