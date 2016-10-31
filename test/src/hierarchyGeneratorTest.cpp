2da269273b211d1d86854d995039fe3c901957f5
lsytj0413 <511121939@qq.com>
2016-10-31 10:25:21 +0800

    GenScatterHierarchy test

--- test/src/hierarchyGeneratorTest.cpp
+++ test/src/hierarchyGeneratorTest.cpp
@@ -29,17 +29,42 @@ TEST_F(HierarchyGenTest, testScatterOne)
     auto v = std::is_same<int, decltype(v1.i)>::value;
     EXPECT_EQ(v, true);
 
+    v = std::is_same<GenScatterHierarchy<int, ScatUnit>, typename T1::LeftBase>::value;
+    EXPECT_EQ(v, true);
+
     using T2 = GenScatterHierarchy<int, ScatUnit>;
     EXPECT_TRUE(sizeof(T2) == sizeof(int));
 
     auto v2 = T2();
     v = std::is_same<int, decltype(v2.i)>::value;
     EXPECT_EQ(v, true);
+
+    v = std::is_same<ScatUnit<int>, typename T2::LeftBase>::value;
+    EXPECT_EQ(v, true);
 }
 
 
 TEST_F(HierarchyGenTest, testScatterMulti)
-{}
+{
+    using T1 = GenScatterHierarchy<Typelist<int, char>, ScatUnit>;
+    using T2 = GenScatterHierarchy<int, ScatUnit>;
+    using T3 = GenScatterHierarchy<Typelist<char>, ScatUnit>;
+
+    auto v = std::is_same<T2, T1::LeftBase>::value;
+    EXPECT_EQ(v, true);
+    v = std::is_same<T3, T1::RightBase>::value;
+    EXPECT_EQ(v, true);
+
+    using T4 = GenScatterHierarchy<Typelist<int, char, double>, ScatUnit>;
+    using T5 = GenScatterHierarchy<int, ScatUnit>;
+    using T6 = GenScatterHierarchy<Typelist<char, double>, ScatUnit>;
+
+    v = std::is_same<T5, T4::LeftBase>::value;
+    EXPECT_EQ(v, true);
+    v = std::is_same<T6, T4::RightBase>::value;
+    EXPECT_EQ(v, true);
+
+}
 
 
 TEST_F(HierarchyGenTest, testFieldT)
