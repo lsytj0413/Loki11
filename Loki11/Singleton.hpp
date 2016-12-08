02fb1a9a4e764c7580db3272f8a6880e506abe25
lsytj0413 <511121939@qq.com>
2016-12-08 19:21:11 +0800

    LifetimeTracker

--- Loki11/Singleton.hpp
+++ Loki11/Singleton.hpp
@@ -10,6 +10,27 @@
 namespace Loki11
 {
 
+namespace Private
+{
 
+class LifetimeTracker
+{
+private:
+    unsigned int m_longevity;
+
+public:
+    LifetimeTracker(unsigned int x):
+            m_longevity(x)
+    {};
+
+    virtual ~LifetimeTracker() = 0;
+
+    static bool Compare(const LifetimeTracker* lhs, const LifetimeTracker* rhs)
+    {
+        return lhs->m_longevity < rhs->m_longevity;
+    }
+};
+
+}
 
 }
