f55f5b2811abb53984d3bc4a979c60120634ee15
lsytj0413 <511121939@qq.com>
2016-12-12 20:16:31 +0800

    FunctorImpl

--- Loki11/Functor.hpp
+++ Loki11/Functor.hpp
@@ -6,8 +6,22 @@
 
 #pragma once
 
+#include "Typelist.hpp"
+
 
 namespace Loki11
 {
+
+template <typename... R>
+class FunctorImpl;
+
+template <typename R, typename... TArgs>
+class FunctorImpl<R, Typelist<TArgs...>>
+{
+public:
+    virtual R operator() (TArgs&&...) = 0;
+    virtual FunctorImpl* Clone() const = 0;
+    virtual ~FunctorImpl() {};
+};
 
 }
