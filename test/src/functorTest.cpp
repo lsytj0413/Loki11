e60c2f124d47f8aecf9c87deee3b033eb7dcf27f
lsytj0413 <511121939@qq.com>
2016-12-14 16:14:46 +0800

    testOriginFunctionOneArgs

--- test/src/functorTest.cpp
+++ test/src/functorTest.cpp
@@ -1,5 +1,8 @@
 #include "functorTest.h"
 
+#include <string>
+using std::string;
+
 #include <Loki11/Functor.hpp>
 using namespace Loki11;
 
@@ -14,4 +17,26 @@ TEST_F(FunctorTest, testOriginFunctionNonArgs)
     auto i = f();
 
     EXPECT_EQ(1, i);
+}
+
+
+int test_oneargs_origin(int i) {
+    return i;
+}
+
+string test_oneargs_origin_str(string i) {
+    return i;
+}
+
+TEST_F(FunctorTest, testOriginFunctionOneArgs)
+{
+    auto f = Functor<int, int>(test_oneargs_origin);
+    auto i = f(1);
+
+    EXPECT_EQ(1, i);
+
+    auto f2 = Functor<string, string>(test_oneargs_origin_str);
+    auto i2 = f2("xxx");
+
+    EXPECT_EQ("xxx", i2);
 }
